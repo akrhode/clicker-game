38ceccfe2e5261d43c70a18ca2988cd596f29c8b
akrhode <a.rhode@web.de>
2022-12-21 16:32:39 +0100

    Add functions

---
+++ script.js
@@ -0,0 +1,37 @@
+var anzahlGegossen = 0;
+
+function giessen() {
+  if (anzahlGegossen < 12) {
+    anzahlGegossen = anzahlGegossen + 1;
+    document.getElementById("bereits-gegossen").innerHTML = anzahlGegossen;
+  } else {
+    alert("Deine Pflanze blüht. Du hast gewonnen!");
+  }
+
+  if (anzahlGegossen == 3) {
+    document
+      .getElementById("kartenbild")
+      .setAttribute("src", "img/Pflanze2.png");
+    document.getElementById("benoetigtes-giessen").innerHTML = 6;
+  }
+
+  if (anzahlGegossen == 6) {
+    document
+      .getElementById("kartenbild")
+      .setAttribute("src", "img/Pflanze3.png");
+    document.getElementById("benoetigtes-giessen").innerHTML = 9;
+  }
+
+  if (anzahlGegossen == 9) {
+    document
+      .getElementById("kartenbild")
+      .setAttribute("src", "img/Pflanze4.png");
+    document.getElementById("benoetigtes-giessen").innerHTML = 12;
+  }
+
+  if (anzahlGegossen == 12) {
+    document
+      .getElementById("kartenbild")
+      .setAttribute("src", "img/Pflanze5.png");
+  }
+}
